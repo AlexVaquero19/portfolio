58ae1401209c8b919231997d8268932c8bccce9f
Alejandro <vaquero.alejandro19@gmail.com>
2022-02-23 08:07:30 +0100

    Changed Slider to swiperjs

--- js/custom.js
+++ js/custom.js
@@ -98,4 +98,18 @@ $(document).ready(function() {
   }
 
   animateText();
+
+  var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
 });
